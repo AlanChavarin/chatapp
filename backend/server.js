0cb536938d45d0ab0450385c7ad0ce61dc4c36e1
Cameron Cobb <cameroncobb411@outlook.com>
2023-11-23 18:32:54 -0800

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbCon');
 const PORT = 5000;
+
+// Connect to MongoDB
+connectDB();
 
 app.get('/', (req, res) => {
     res.json({
@@ -9,6 +14,12 @@ app.get('/', (req, res) => {
     })
 });
 
-app.listen(PORT, () => {
-    console.log('Server started on port ${PORT}');
-});
+// Listen for requests i we have successully connected to MongoDB. This is when
+// open evenet is emitted by the mongoose connection.
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+        console.log('Server started on port ${PORT}');
+    });
+})
+
